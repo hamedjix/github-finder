bdeb9242dadaeda67bd1e3a31de80224698853f2
Hamed Jafari <hamedjix@gmail.com>
2021-03-15 00:10:47 +0330

    search github users complete

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
     loading: false,
   };
   async componentDidMount() {
+    console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
     console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
     this.setState({ loading: true });
     const response = await axios.get(
@@ -19,12 +20,20 @@ class App extends Component {
 
     this.setState({ users: response.data, loading: false });
   }
+  //Search Github Users
+  searchUsers = async (text) => {
+    const response = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: response.data.items, loading: false });
+  };
   render() {
     return (
       <div className='App'>
         <Navbar title='Github Finder' />
         <div className='container'>
-          <Search />
+          <Search onSearch={this.searchUsers} />
           <Users users={this.state.users} />
         </div>
       </div>
--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -5,19 +5,27 @@ class Search extends Component {
   state = {
     text: '',
   };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSearch(this.state.text);
+    this.setState({ text: '' });
+  };
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
   render() {
     return (
-      <div>
-        <form>
-          <input
-            className='Search'
-            type='text'
-            name='text'
-            placeholder='Search Users'
-            value={this.state.text}
-          />
-        </form>
-      </div>
+      <form onSubmit={this.onSubmit} className='Search'>
+        <input
+          type='text'
+          name='text'
+          placeholder='Search Users'
+          value={this.state.text}
+          onChange={this.onChange}
+        />
+        <button type='submit'>Search</button>
+      </form>
     );
   }
 }
